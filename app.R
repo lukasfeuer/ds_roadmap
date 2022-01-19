3193fd82ac63d5ddd4c4ee4097bb6dd401e143d5
Lukas Feuer <feuerlukas@web.de>
2022-01-19 05:56:18 +0100

    initial commit

---
+++ app.R
@@ -0,0 +1,129 @@
+# =============================================================================:
+# Project Roadmap -------------------------------------------------------------
+# =============================================================================:
+
+# TODO
+#   * Text boxes have to save input
+#   * Changes in time line items have to be saved
+#   * Add reactive display of individual project model period
+#   * check for missing time stamps / include the timing systematic dependent on
+#     initial dates of presentation start value
+
+library(shiny)
+library(tidyverse)
+library(timevis)
+
+proj <- read_csv2("data/project_list.csv",
+                  na = "NA",
+                  locale = locale(date_format = c("%d.%m.%y")))
+
+timevisData <- proj %>% select(-c(kommentar, model_type))
+
+timevisDataGroups <- proj %>%
+    select(-c(model_type, kommentar)) %>%
+    filter(type == "range") %>%
+    transmute(id = group,
+              content = content) %>%
+    data.frame()
+
+# maybe redundant 
+proj_groups <- proj %>%
+    distinct(group) %>%
+    drop_na() %>%
+    .[[1]]
+
+
+# UI ---------------------------------------------------------------------------
+
+ui <- fluidPage(
+    ### CSS ----
+    tags$head(
+        tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
+    ),
+    
+    ### Animated Background ----
+    tagList(tags$head(
+        tags$script(src = "https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.min.js")
+    ),
+    includeScript("www/app.js")),
+    div(id = 'particles-js'),
+    
+    # Application title
+    fluidRow(titlePanel(
+        h1("Projects | Data Science"), windowTitle = "Projects"
+    )),
+    
+    fluidRow(
+        column(width = 4,
+               h3("Projects"),
+               hr(),
+               wellPanel(uiOutput('dynamic')), ),
+        column(width = 8,
+               h3("Time Line"),
+               hr(),
+               wellPanel(timevisOutput("timeline")))
+    )
+    
+)
+
+
+# Server -----------------------------------------------------------------------
+
+server <- function(input, output) {
+    
+    output$timeline <- renderTimevis({
+        config <- list(
+            editable = TRUE,
+            align = "center",
+            orientation = "top",
+            snap = NULL,
+            margin = list(item = 30, axis = 50)
+        )
+        timevis(
+            data = timevisData,
+            groups = timevisDataGroups,
+            options = config,
+            zoomFactor = 5
+        )
+    })
+    
+    # Generates code for individual info panel
+    make_panel <- function(x, group, i) {
+        x <- x %>%
+            filter(group == UQ(group) &
+                       !is.na(model_type)) %>%
+            select(content, start, end, kommentar, model_type) %>%
+            mutate(across(everything(), ~ as.character(.))) %>%
+            pivot_longer(cols = everything(),
+                         names_to = "var",
+                         values_to = "value")
+        
+        wellPanel(
+            class = "info_box",
+            h4(x$value[x$var == "content"]),
+            x$value[x$var == "model_type"],
+            hr(),
+            tags$b("Model Period:"),
+            tags$i("\nfrom", x$value[x$var == "start"],
+                   "\nto", x$value[x$var == "start"]),
+            br(),
+            textAreaInput(
+                inputId = "kommentar_input",
+                label = "Comments:",
+                value = x$value[x$var == "kommentar"]
+            )
+        )
+    }
+    
+    output$dynamic <- renderUI({
+        tags <- tagList()
+        
+        for (i in 1:length(proj_groups)) {
+            tags[[i]] <- make_panel(proj, proj_groups[i], i)
+        }
+        
+        tags
+    })
+}
+
+shinyApp(ui = ui, server = server)
